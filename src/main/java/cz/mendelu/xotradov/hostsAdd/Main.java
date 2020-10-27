40627cf05b92c740bef11d2e33a62eee3cdc357f
otradovec <otradovec@github.com>
2020-10-27 23:10:19 +0100

    Fully working

--- src/main/java/cz/mendelu/xotradov/hostsAdd/Main.java
+++ src/main/java/cz/mendelu/xotradov/hostsAdd/Main.java
@@ -1,11 +1,16 @@
 package cz.mendelu.xotradov.hostsAdd;
 
 import java.io.*;
+import java.nio.channels.FileChannel;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Main {
-    // ne opakovani
+    ArrayList<String> inputList = new ArrayList<>();
     //podle abecedy
+    //nacist nove radky usporadane
+    //chanelem doplnit
      public static void main(String[] args) {
          new Main().start(args[0],args[1]);
          System.out.println("Input file: "+ args[0]);
@@ -19,12 +24,6 @@ public class Main {
     }
 
     public void start(File inputFile, File hostsFile){
-        if (inputFile==null){
-            System.out.println("Nothing1!!!!!!!!!!1");
-        }else {
-            System.out.println(inputFile.getName());
-            System.out.println(inputFile.getAbsolutePath());
-        }
         if (inputFile.exists()) {
             if (inputFile.canRead()){
                 if (hostsFile.exists()) {
@@ -47,34 +46,106 @@ public class Main {
         try (FileInputStream inputStream = new FileInputStream(inputFile); Scanner sc = new Scanner(inputStream, "UTF-8")) {
             while (sc.hasNextLine()) {
                 String line = sc.nextLine();
-                addLine(line,hostsFile);
+                String cleanedLine = getCleaned(line);
+                String rightLine= getHostsTypeLine(cleanedLine);
+                if (!isPresent(rightLine,hostsFile)){
+                    addLine(rightLine);
+                }
             }
             // note that Scanner suppresses exceptions
             if (sc.ioException() != null) {
                 throw sc.ioException();
             }
         } catch (IOException ignored) {
+            System.err.println("IO exception while reading a file");
+        }
+        Collections.sort(inputList);
+        addLines(hostsFile);
+    }
 
+    private void addLines(File hostsFile) {
+        RandomAccessFile r = null;
+        RandomAccessFile rtemp = null;
+        try {
+            //ToDo just read
+             r = new RandomAccessFile(hostsFile, "rw");
+             File tempFile = new File(hostsFile.getName() + "~");
+             tempFile.delete();
+             rtemp = new RandomAccessFile(tempFile, "rw");
+            r.seek(0);
+            String line = r.readLine();
+            while ((line) != null && !inputList.isEmpty()){
+                //input is lower
+                if (isFirstBeforeSecond(inputList.get(0),line)){
+                    rtemp.write((inputList.get(0)+"\n").getBytes());
+                    inputList.remove(0);
+                }else {
+                    //line is lower
+                    rtemp.write((line+"\n").getBytes());
+                    line = r.readLine();
+                }
+            }
+            //one of the files might have unresolved hosts
+            if ((line) != null){
+                while ((line) != null || !inputList.isEmpty()){
+                    rtemp.write((line+"\n").getBytes());
+                    line = r.readLine();
+                }
+            }else if (!inputList.isEmpty()){
+                while ((line) != null || !inputList.isEmpty()){
+                    rtemp.write((inputList.get(0)+"\n").getBytes());
+                    inputList.remove(0);
+                }
+            }
+            System.out.println("Just");
+            r.close();
+            rtemp.close();
+            //copy to
+            System.out.println("Just2");
+            copyFileUsingChannel(tempFile,hostsFile);
+        } catch (Exception e) {
+            try {
+                r.close();
+                rtemp.close();
+            } catch (Exception e1){
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    public boolean isFirstBeforeSecond(String first, String second) {
+        return first.compareTo(second)<0;
+    }
+
+    private void copyFileUsingChannel(File source, File dest) throws IOException {
+        FileChannel sourceChannel = null;
+        FileChannel destChannel = null;
+        try {
+            sourceChannel = new FileInputStream(source).getChannel();
+            destChannel = new FileOutputStream(dest).getChannel();
+            destChannel.transferFrom(sourceChannel, 0, sourceChannel.size());
+        }finally{
+            sourceChannel.close();
+            destChannel.close();
+        }
+    }
+    private boolean isPresent(String rightLine, File hostsFile) {
+        try (FileInputStream inputStream = new FileInputStream(hostsFile); Scanner sc = new Scanner(inputStream, "UTF-8")) {
+            while (sc.hasNextLine()) {
+                String line = sc.nextLine();
+                if (line.equals(rightLine)){
+                    return true;
+                }
+            }
+            return false;
+        } catch (IOException ignored) {
+            return false;
         }
     }
 
-    private void addLine(String line, File hostsFile) {
-         String cleanedLine = getCleaned(line);
-         String rightLine= getHostsTypeLine(cleanedLine);
-        try {
-            String loc = hostsFile.getCanonicalPath();
-            FileWriter fstream = new FileWriter(loc, true);
-            BufferedWriter out = new BufferedWriter(fstream);
-            System.out.println("Adding line: "+rightLine);
-            out.write(rightLine);
-            out.newLine();
-
-            //close buffer writer
-            out.close();
-            fstream.close();
-        } catch (IOException e) {
-            System.out.println("Failed to write new lines to the hosts file.");
-        }
+    private void addLine(String rightLine) {
+        inputList.add(rightLine);
     }
 
     private String getCleaned(String line){
--- src/test/java/cz/mendelu/xotradov/hostsAdd/MainTest.java
+++ src/test/java/cz/mendelu/xotradov/hostsAdd/MainTest.java
@@ -1,12 +1,26 @@
 package cz.mendelu.xotradov.hostsAdd;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class MainTest {
 
+    @Test
+    public void isFirstBeforeSecond() {
+        Main main = new Main();
+        assertTrue(main.isFirstBeforeSecond("0.0.0.0 a.cz","0.0.0.0 b.cz"));
+        assertTrue(main.isFirstBeforeSecond("0.0.0.0 a.eu","0.0.0.0 b.cz"));
+        assertTrue(main.isFirstBeforeSecond("0.0.0.0 b.cz","0.0.0.0 b.eu"));
+        assertTrue(main.isFirstBeforeSecond("0.0.0.0 azzzzzz.cz","0.0.0.0 b.cz"));
+        assertFalse(main.isFirstBeforeSecond("0.0.0.0 b.cz","0.0.0.0 b.cz"));
+        assertFalse(main.isFirstBeforeSecond("0.0.0.0 c.cz","0.0.0.0 b.cz"));
+        assertFalse(main.isFirstBeforeSecond("0.0.0.0 bbbb.bbb.bb","0.0.0.0 a.cz"));
+    }
 
     @Test
     public void start() {
         new Main().start( "src/test/resources/cz/mendelu/xotradov/hostsAdd/newHosts.txt", "src/test/resources/cz/mendelu/xotradov/hostsAdd/hosts.txt");
     }
+
+
 }
